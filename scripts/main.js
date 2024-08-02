c32e383ec974893c29447347ea7d6d2e578ee5a2
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-08-02 17:03:08 -0400

    made the footer with the contacts linked also made the 3rd page

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to(".profileText", {
+    gsap.to("profileText1", {
         opacity: 1,
         x: 0,
         duration: 1,
         ease: "power2.out",
         scrollTrigger: {
-            trigger: ".profileTextContainer",
+            trigger: ".profileText1Container",
             start: "top 80%",
             end: "bottom 20%",
             scrub: true,
             toggleActions: "play none none reverse",
-            onEnter: () => gsap.to(".profileText", { opacity: 1, x: 0, duration: 1 }),
-            onLeave: () => gsap.to(".profileText", { opacity: 0, x: 100, duration: 1 }),
-            onEnterBack: () => gsap.to(".profileText", { opacity: 1, x: 0, duration: 1 }),
-            onLeaveBack: () => gsap.to(".profileText", { opacity: 0, x: 100, duration: 1 })
+            onEnter: () => gsap.to(".profileText1", { opacity: 1, x: 0, duration: 1 }),
+            onLeave: () => gsap.to(".profileText1", { opacity: 0, x: 100, duration: 1 }),
+            onEnterBack: () => gsap.to(".profileText1", { opacity: 1, x: 0, duration: 1 }),
+            onLeaveBack: () => gsap.to(".profileText1", { opacity: 0, x: 100, duration: 1 })
         }
     });
+
+    gsap.to(".profileText2", {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".profileText2Container",
+            start: "top 80%",
+            end: "bottom 20%",
+            scrub: true,
+            toggleActions: "play none none reverse",
+            onEnter: () => gsap.to(".profileText2", { opacity: 1, x: 0, duration: 1 }),
+            onLeave: () => gsap.to(".profileText2", { opacity: 0, x: -100, duration: 1 }),
+            onEnterBack: () => gsap.to(".profileText2", { opacity: 1, x: 0, duration: 1 }),
+            onLeaveBack: () => gsap.to(".profileText2", { opacity: 0, x: -100, duration: 1 })
+        }
+    });
+    
 });
