8e176957c5019c28b0c1120b6fc84bd913ebd9ef
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-08-20 12:55:21 -0400

    made a scroll effect when you clicked hotbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,37 @@
 
 AOS.init();
+
+
+
+document.querySelector('.aboutMe').addEventListener('click', function() {
+    document.getElementById('aboutMe').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
+
+document.querySelector('.resume').addEventListener('click', function() {
+    document.getElementById('resume').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
+
+
+document.querySelector('.projects').addEventListener('click', function() {
+    document.getElementById('projects').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
+
+
+document.querySelector('.experience').addEventListener('click', function() {
+    document.getElementById('timeline').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
+
+
+document.querySelector('.abilities').addEventListener('click', function() {
+    document.getElementById('skills').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+});
