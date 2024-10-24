dc459f7f85ad2cdd6fc745d4a9acb17cd09ce5ad
BriannLam <126619419+BriannLam@users.noreply.github.com>
2024-10-24 16:25:41 -0400

    finished the hamburger menu in both desktop and mobile

--- scripts/main.js
+++ scripts/main.js
@@ -16,56 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log("unclicked!");
     });
 });
-
-
-document.querySelector('.aboutMe').addEventListener('click', function() {
-    document.getElementById('aboutMe').scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-});
-
-document.querySelector('.resume').addEventListener('click', function() {
-    document.getElementById('resume').scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-});
-
-
-document.querySelector('.projects').addEventListener('click', function() {
-    document.getElementById('projects').scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-});
-
-
-document.querySelector('.experience').addEventListener('click', function() {
-    document.getElementById('timeline').scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-});
-
-
-document.querySelector('.abilities').addEventListener('click', function() {
-    document.getElementById('skills').scrollIntoView({ 
-        behavior: 'smooth' 
-    });
-});
-
-
-$(document).ready(function() {
-    $('.burger-wrap').click(function() {
-        $('body').toggleClass('nav-open'); 
-        console.log("clicked!");
-    });
-  
-    $('.menu-cover').click(function() {
-        $('body').removeClass('nav-open');
-        console.log("unclicked!");
-    });
-  });
   
 
-  function myFunction() {
+  function scrollAboutMe() {
     const aboutMe = document.getElementById("aboutMe");
     aboutMe.scrollIntoView();
+  }
+  function scrollResume() {
+    const resume = document.getElementById("resume");
+    resume.scrollIntoView();
+  }
+  function scrollProject() {
+    const project = document.getElementById("project");
+    project.scrollIntoView();
+  }
+  function scrollSkills() {
+    const skills = document.getElementById("skills");
+    skills.scrollIntoView();
   }
